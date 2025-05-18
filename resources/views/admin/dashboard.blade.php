4b3030820f31e1b26129cfef51476f6dcd734ba4
QuynhNguyen <nguyentuanquynh101203@gmail.com>
2025-05-18 23:26:23 +0700

    custom vite

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,5 +1,18 @@
-<h1>Chào mừng Admin!</h1>
-<form action="{{ route('admin.logout') }}" method="POST">
-    @csrf
-    <button type="submit">Đăng xuất</button>
-</form>
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Admin Dashboard</title>
+    @vite('resources/scss/admin-style.scss')
+</head>
+<body>
+<header>
+    <h1>Chào mừng Admin!</h1>
+    <form action="{{ route('admin.logout') }}" method="POST">
+        @csrf
+        <button type="submit">Đăng xuất</button>
+    </form>
+</header>
+</body>
+</html>
