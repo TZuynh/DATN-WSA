2506b952d1ad68528bd193e069c0f580148b95bd
QuynhNguyen <nguyentuanquynh101203@gmail.com>
2025-05-20 20:54:12 +0700

    fix bug login

--- routes/admin/auth.php
+++ routes/admin/auth.php
@@ -3,6 +3,6 @@
 use App\Http\Controllers\Admin\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [AuthController::class, 'showLoginForm'])->name('admin.login');
-Route::post('/', [AuthController::class, 'login'])->name('admin.login.submit');
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('admin.login');
+Route::post('/login', [AuthController::class, 'login'])->name('admin.login.submit');
 Route::post('/logout', [AuthController::class, 'logout'])->name('admin.logout');
