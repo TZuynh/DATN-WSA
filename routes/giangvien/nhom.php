f3e54c9af460a0ee1c689a6ea79584a509f34d4f
QuynhNguyen <nguyentuanquynh101203@gmail.com>
2025-06-02 23:52:10 +0700

    update

--- routes/giangvien/nhom.php
+++ routes/giangvien/nhom.php
@@ -2,13 +2,8 @@
 use App\Http\Controllers\GiangVien\NhomController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('nhom')->name('giangvien.nhom.')->middleware(['auth', 'role:giang_vien'])->group(function () {
-    Route::get('/', [NhomController::class, 'index'])->name('index');
-    Route::get('/create', [NhomController::class, 'create'])->name('create');
-    Route::post('/store', [NhomController::class, 'store'])->name('store');
-    Route::get('/{nhom}/edit', [NhomController::class, 'edit'])->name('edit');
-    Route::put('/{nhom}', [NhomController::class, 'update'])->name('update');
-    Route::delete('/{nhom}', [NhomController::class, 'destroy'])->name('destroy');
-    Route::post('/import', [NhomController::class, 'import'])->name('import');
-    Route::get('/download-template', [NhomController::class, 'downloadTemplate'])->name('download-template');
+Route::middleware(['auth', 'role:giang_vien'])->name('giangvien.')->group(function () {
+    Route::resource('/giang-vien/nhom', NhomController::class)->except(['show']);
+    Route::post('/giang-vien/nhom/import', [NhomController::class, 'import'])->name('nhom.import');
+    Route::get('/giang-vien/nhom/download-template', [NhomController::class, 'downloadTemplate'])->name('nhom.download-template');
 });
